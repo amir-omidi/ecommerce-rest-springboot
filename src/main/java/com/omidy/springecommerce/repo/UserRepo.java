a31f5b3ddf7af8c7db166b4f494da1b706c13183
Amirhossein <amir.ho.omidy@gmail.com>
2025-05-27 10:21:48 -0600

    Add Spring Security config and basic login functionality

---
+++ src/main/java/com/omidy/springecommerce/repo/UserRepo.java
@@ -0,0 +1,11 @@
+package com.omidy.springecommerce.repo;
+
+import com.omidy.springecommerce.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer> {
+
+    User findByUsername(String username);
+}
